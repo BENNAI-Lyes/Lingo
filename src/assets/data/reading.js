e060a4ddf661a51ad745f6a62730b8eba6511115
nesrin41 <121663976+nesrin41@users.noreply.github.com>
2022-12-30 10:05:40 +0100

    Update reading.js

--- src/assets/data/reading.js
+++ src/assets/data/reading.js
@@ -181,42 +181,5 @@ export const reading = [
       },
     ],
   },
-  {
-    id: 2,
-    title:
-      'sequence 02: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-  },
-  {
-    id: 3,
-    title: 'sequence 03: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-  },
-  {
-    id: 4,
-    title: 'sequence 04: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-  },
-  {
-    id: 5,
-    title: 'sequence 05: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-  },
-  {
-    id: 6,
-    title: 'sequence 06: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-  },
-  {
-    id: 7,
-    title: 'sequence 07: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-  },
-  {
-    id: 8,
-    title:
-      'sequence 08: lorem dfgdfg rtgrgd dfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfledfgfgdfg fdddfffff fffffffffff df',
-  },
-  {
-    id: 9,
-    title: 'sequence 09: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-  },
-  {
-    id: 10,
-    title: 'sequence 10: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-  },
+ 
 ];
