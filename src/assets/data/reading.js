96b6387f3913528d85b6470709fd425cc6613666
nesrin41 <121663976+nesrin41@users.noreply.github.com>
2023-03-11 20:51:57 +0100

    Update reading.js

--- src/assets/data/reading.js
+++ src/assets/data/reading.js
@@ -2,7 +2,7 @@ export const reading = [
   {
     id: 1,
     title:
-      'sequence 01: reading fdddfffff fffffffffm dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dflorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffm dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffm dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
+      'sequence 01: Sequence 01: Itineraries: My Extraordinary Trip “part 01”',
     cat: 'reading',
     audioUrl: 'audioReading_1',
     videoUrl:
@@ -10,11 +10,11 @@ export const reading = [
     test: 'r-t-1',
     hint: 'lorem dfgdfg lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg rtgrgd dfgfgdfg fdddfffff ffffffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdf ff lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence',
     text: [
-      'sequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff lorem dfgdfg rtgrgd dfgfgdfg fdddfffff ffffffg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
-      ' dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
+      'Our school organized a seven-days trip around Algeria for excellent learners. The departure was from our city; we all gathered near the school with our parents waiting for the couch, but before heading there, I made sure that all the necessary items (my mobile phone, money, my food, my clothes …etc) were all well-packed. Our final destination was Tlemcen with a stop-over for 2 nights in the capital city, Algiers. ',
+      ' First, we travelled to Algiers. On the first day, we went on a sightseeing tour around the city. We visited Makam Elchahid and the National Museum of Moudjahid. The latter is a masterpiece located directly beneath Makam Chahid; it aims to collect, preserve and display objects and memories of the struggle against the French colonialism. At the entrance, the guards asked us to turn off our mobile-phones; therefore, I couldn’t take any pictures of that wonderful place. On the second day, we headed to another tourist attraction which was Elhamma Garden also known as “The Botanical Garden”. At first sight, I felt like “Alice in Wonderland”; it’s so huge and there are hundreds of different plants and high trees. ',
       ' dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff df',
     ],
-    exp: 'fgfgdfg fdddfffff fffffffffff dfsequence 01: lorem dfgdfg rtgrgd dfgfgdfg f',
+    exp: 'a coach: is a type of bus for travelling long distances',
     questions: [
       // true false
       {
@@ -22,25 +22,25 @@ export const reading = [
         list: [
           {
             question:
-              'have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet, consectetur?',
+              '1.	They had a one weak trip. ',
+            answers: ['True', 'False'],
+            correctAnswer: 'True',
+          },
+          {
+            question:
+              '2.	They didn’t take any luggage with them. ',
             answers: ['True', 'False'],
             correctAnswer: 'False',
           },
           {
             question:
-              'have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet, consectetur?',
+              '3.	They visited Makam Chahid and El Hamma Garden and the National Museum.',
             answers: ['True', 'False'],
-            correctAnswer: 'False',
+            correctAnswer: 'True',
           },
           {
             question:
-              'have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet, consectetur?',
-            answers: ['True', 'False'],
-            correctAnswer: 'False',
-          },
-          {
-            question:
-              'have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ipsum dolor sit amet, consectetur?',
+              '4.	The pupils were allowed to take pictures in the museum. ',
             answers: ['True', 'False'],
             correctAnswer: 'False',
           },
@@ -58,58 +58,43 @@ export const reading = [
         type: 'multipleChoice',
         list: [
           {
-            question: 'Choose the right choice ?',
+            question: '•	This trip was organized by: ',
             answers: [
-              '1- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '2- have copmleat Lorem ipsum dolor sit amet,have ',
-              '3- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '4- have copmleat Lorem ipsum dolor sit amet,have copmleat Loremhave copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '5- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '6- have copmleat Lorem ipsum dolor sit amet',
-              '7- have copmleat Lorem ipsum dolor sit amet sdfsdf fdgdg fg',
-              '8- Lorem ipsum dolor sit amet',
-              '9- have copmleat Lorem  dolor sit amet',
-              '10- have copmleat Lorem ipsum dolor sit amet',
-              '11- have copmleat Lorem ipsum dolor sit amet',
-              '12- have copmleat Lorem ipsum dolor sit amet',
+              '1- The school',
+              '2- an organization ',
+              '3- The parents ',
+              '4- The sports club',
             ],
-            correctAnswer: '12- have copmleat Lorem ipsum dolor sit amet',
+            correctAnswer: '1- The school',
           },
           {
-            question: 'Choose the right choice ?',
+            question: '•	The learners gathered near the school: ',
             answers: [
-              '1- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '2- have copmleat Lorem ipsum dolor sit amet,have ',
-              '3- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '4- have copmleat Lorem ipsum dolor sit amet,have copmleat Loremhave copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '5- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '6- have copmleat Lorem ipsum dolor sit amet',
+              '1- 	on their own',
+              '2- 	with their teachers ',
+              '3- 	with their parents',
+              '4- 	with their neighbours ',
             ],
-            correctAnswer: '6- have copmleat Lorem ipsum dolor sit amet',
+            correctAnswer: '3- with their parents',
           },
           {
-            question: 'Choose the right choice ?',
+            question: '•	The cities they visited are: ',
             answers: [
-              '1- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '2- have copmleat Lorem ipsum dolor sit amet,have ',
-              '3- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '4- have copmleat Lorem ipsum dolor sit amet,have copmleat Loremhave copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '5- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '6- have copmleat Lorem ipsum dolor sit amet',
+              '1- 	Tlemcen then Algiers ',
+              '2- 	Tlemcen only  ',
+              '3- 	Algiers onlyt',
+              '4- 	Algiers then Tlemcen ',
             ],
-            correctAnswer: '6- have copmleat Lorem ipsum dolor sit amet',
+            correctAnswer: '4- 	Algiers then Tlemcen ',
           },
           {
-            question: 'Choose the right choice ?',
+            question: '•	The National Museum is: ',
             answers: [
-              '1- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '2- have copmleat Lorem ipsum dolor sit amet,have ',
-              '3- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '4- have copmleat Lorem ipsum dolor sit amet,have copmleat Loremhave copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '5- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-              '6- have copmleat Lorem ipsum dolor sit amet',
+              '1- a collection of artistic famous paintings.',
+              '2- a collection of rare stones ',
+              '3- a collection of objects and memories related to the colonialism ',
             ],
-            correctAnswer: '6- have copmleat Lorem ipsum dolor sit amet',
+            correctAnswer: '3- a collection of objects and memories related to the colonialism',
           },
           {
             question: 'Choose the right choice ?',
@@ -130,25 +115,25 @@ export const reading = [
       {
         type: 'matching',
         question:
-          'have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem ?',
+          'read the text again and match each word with its definition ',
         answers: {
           g1: [
-            '1- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-            '2- have copmleat Lorem ipsum dolor sit amet,have ',
-            '3- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
-            '4- have copmleat Lorem ipsum dolor sit amet,have copmleat Loremhave copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
+            '1-	The departure',
+            '2- Final Destination ',
+            '3- Garden',
+            '4-	tourist attraction',
             '5- have copmleat Lorem ipsum dolor sit amet,have copmleat Lorem',
           ],
           g2: [
-            'a- have copmleat Lorem ipsum dolor sit amet',
-            'b- have copmleat Lorem ipsum dolor sit amet sdfsdf fdgdg fg',
-            'c- Lorem ipsum dolor sit amet',
-            'd- have copmleat Lorem  dolor sit amet',
+            'a- a place full of plants and trees ',
+            'b- a place that you start your trip from',
+            'c- a place people like to visit during holidays because it is beautiful and famous.',
+            'd- a place you visit at the end of the trip ',
             'e- have copmleat Lorem ipsum dolor sit amet',
           ],
         },
 
-        correctAnswer: ['1-a', '2-b', '3-c', '4-d', '5-e'],
+        correctAnswer: ['1-b', '2-d', '3-a', '4-c', '5-e'],
       },
 
       //re-ordering
@@ -173,7 +158,7 @@ export const reading = [
       //filling the gaps
       {
         type: 'fillingGaps',
-        question: 'fill the gaps with: hhhh-sdsdsd-sdsd-sd-fdfdf ?',
+        question: ' Read the text again and fill in the gaps with “National Museum/ Algiers/ Elhamma Garden/ the pupils',
         answers: [
           ' ----- ham dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfgdfg rtgrgd dfgfgdfg fdddffe ------ copmlem dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfgdfg rtgrgd dfgfgdfg fdddffat Lorem ----- ipsum dolor ----- sit am dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfgdfg rtgrgd dfgfgdfg fdddffmet,have ----- copmleatm dfgdfg rtgrgd dfgfgdfg fdddfffff fffffffffff dfgdfg rtgrgd dfgfgdfg fdddff Lorem',
         ],
